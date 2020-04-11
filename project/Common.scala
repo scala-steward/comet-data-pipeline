1d496d1109a17c5b6641376d223e3f599ed7fb1c
Scala Steward <me@scala-steward.org>
2020-04-11 22:14:56 +0200

    Update jackson-databind to 2.9.10.4

--- project/Common.scala
+++ project/Common.scala
@@ -110,7 +110,7 @@ object Versions {
   val log4s = "1.3.3"
   val betterFiles = "3.8.0"
   val jackson211 = "2.7.9"
-  val jackson212 = "2.9.10"
+  val jackson212 = "2.9.10.4"
   val configs = "0.4.4"
   val esHadoop = "7.6.2"
   val scopt = "4.0.0-RC2"
